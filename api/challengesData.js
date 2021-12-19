e30d1e9628584ec8275feb3d87e92f956a39f447
Ashby Hernandez <asashby@hotmail.com>
2021-12-19 18:40:36 -0400

    frequency null

--- api/challengesData.js
+++ api/challengesData.js
@@ -7,7 +7,7 @@ const challenges = [
 		days: 60,
 		level: 'Principiante',
 		slug: 'basico-en-casa',
-		frequency: 'Diaria'
+		frequency: null
 	},
 	{
 		id: 18,
@@ -27,7 +27,7 @@ const challenges = [
 		days: 40,
 		level: 'Avanzado',
 		slug: 'avanzado-en-gym',
-		frequency: 'Diaria'
+		frequency: null
 	}
 ]
 
