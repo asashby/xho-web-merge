de30ae973677bc90921ea66d20116f34707e3511
Ashby Hernandez <asashby@hotmail.com>
2022-01-05 22:59:42 -0400

    culqi acces-control header added

--- plugins/culqiAxios.js
+++ plugins/culqiAxios.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 const culqiHttpClient = axios.create({
 	baseURL: 'https://api.culqi.com/v2',
 	headers: {
-		'Content-Type': 'application/json'
+		'Content-Type': 'application/json',
+		'Access-Control-Allow-Credentials': 'true'
 		// anything you want to add to the headers
 	}
 })
