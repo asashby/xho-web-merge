82e543a2ec4deddd8a093a50b289c6afadeda62e
rrosas1198 <rrosas1198@gmail.com>
2022-02-22 21:46:56 -0500

    changes v0.0.28

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ export default {
 			home: '/objetivos',
 			login: '/', // INFO: Probar con /
 			logout: '/', // INFO: Probar con /
-			callback: false // TODO: Cambiar por /objetivos o /perfil o /
+			callback: '/objetivos' // TODO: Cambiar por /objetivos o /perfil o /
 		},
 		strategies: {
 			local: {
